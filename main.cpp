4e4bf05bd3caf9a7256703fd940add0338b48eb2
sarcinelliRenan <sarcinellirenan@gmail.com>
2017-06-06 10:50:06 -0300

    timestamp fps

--- main.cpp
+++ main.cpp
@@ -4,12 +4,15 @@
 #include "opencv2/video/background_segm.hpp"
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
+#include <chrono>
 
 using namespace cv;
 using namespace std;
 
+typedef chrono::high_resolution_clock Timer;
+
 void set_parameters (VideoCapture* camera){
-    camera->set(CV_CAP_PROP_FPS , 10);
+    camera->set(CV_CAP_PROP_FPS , 30);
     camera->set(CV_CAP_PROP_FRAME_HEIGHT, 640);
     camera->set(CV_CAP_PROP_FRAME_WIDTH, 480);
 }
@@ -21,11 +24,17 @@ int main() {
 
     set_parameters(&video);
 
+    chrono::time_point<chrono::system_clock> start, end;
+    start = Timer::now();
+
     for(;;)
     {
         Mat frame;
+        end = Timer::now();
+        cout << "FPS : " << (double)1000/(chrono::duration_cast<chrono::milliseconds> (end - start).count()) << std::endl;
+        start = end;
         video >> frame; // get a new frame from camera
-        imshow("!!OLHA O VIADO!!", frame);
+        imshow("Preview", frame);
         if(waitKey(30) >= 0) break;
     }
 
